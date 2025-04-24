88d69027708d0141858358b1ed18b478b44ae6d4
MatiasDG539 <gutierrezmatiasdaniel539@gmail.com>
2025-04-24 15:47:30 -0300

    fix: reorder FeaturedProperties section for better layout flow

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <main className="flex-1">
         <HeroSection />
         <AboutSection />
-        <FeaturedProperties />
         <BrowseWebsiteSection />
+        <FeaturedProperties />
         <TestimonialsSection />
         <ContactSection />
       </main>
